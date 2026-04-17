2010d8e5014b050a263b1c2bdb710fc8676cd771
Victor Guzmán <156972328+victorguz@users.noreply.github.com>
2026-04-17 18:58:59 -0500

    -

--- src/main.ts
+++ src/main.ts
@@ -9,24 +9,24 @@ import * as requestIp from 'request-ip';
 
 import { AppModule } from './app/app.module';
 
-const cors = {
-  origin: getOrigin(),
-  methods: 'POST,OPTIONS,GET,PUT,PATCH,DELETE',
-  allowedHeaders:
-    'Content-Type, Accept, Authorization, X-Requested-With, Application, Origin, Access-Control-Allow-Origin, Access-Control-Allow-Credentials',
-};
+// const cors = {
+//   origin: getOrigin(),
+//   methods: 'POST,OPTIONS,GET,PUT,PATCH,DELETE',
+//   allowedHeaders:
+//     'Content-Type, Accept, Authorization, X-Requested-With, Application, Origin, Access-Control-Allow-Origin, Access-Control-Allow-Credentials',
+// };
 
-function getOrigin() {
-  switch (process.env.NODE_ENV) {
-    case 'prd':
-      return 'https://app.vyvapos.com';
-    case 'dev':
-    case 'qas':
-    default:
-      return '*';
-    // return 'https://qas.d2mrz2vv88ypo1.amplifyapp.com';
-  }
-}
+// function getOrigin() {
+//   switch (process.env.NODE_ENV) {
+//     case 'prd':
+//       return 'https://app.vyvapos.com';
+//     case 'dev':
+//     case 'qas':
+//     default:
+//       return '*';
+//     // return 'https://qas.d2mrz2vv88ypo1.amplifyapp.com';
+//   }
+// }
 
 async function bootstrap(
   expressApp: Express | undefined = undefined,
@@ -46,7 +46,7 @@ async function bootstrap(
   app.setGlobalPrefix('api');
   app.use(json({ limit: '10mb' }));
   app.use(urlencoded({ extended: true, limit: '10mb' }));
-  app.enableCors(cors);
+  // app.enableCors(cors);
 
   // Enable DTO validations
   app.useGlobalPipes(
